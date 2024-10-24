bab407e211769742e5d0e9d2934721f6fdb0d991
only-left <hnczj99@gmail.com>
2024-10-25 01:42:40 +1100

    modify index page

--- index_files/love.js
+++ index_files/love.js
@@ -169,7 +169,7 @@
             ctx.moveTo(0, 0);
             ctx.scale(0.75, 0.75);
             ctx.font = "12px 微软雅黑,Verdana"; 
-            ctx.fillText("奥特曼!!!", 23, 10);
+            ctx.fillText("葱妹和蒜姐", 23, 10);
             ctx.restore();
         },
         clear: function() {
